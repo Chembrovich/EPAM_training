4170b7a9929b6259a116f260cc1c43e669c22699
Chembrovich <andrey.chembrovich@gmail.com>
2017-03-18 22:31:19 +0300

    Basic

---
+++ src/com/company/enums/Category.java
@@ -0,0 +1,8 @@
+package com.company.enums;
+
+/**
+ * Created by Андрей on 18.03.2017.
+ */
+public enum Category {
+    FOOTBALL, VOLLEYBALL, BASEBALL, GYMNASTICS, DARTS, HOCKEY, SWIMMING, TENNIS, CLOTHES, SHOES, OTHER
+}
